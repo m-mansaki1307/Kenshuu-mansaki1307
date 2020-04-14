bc7bdf5bf2832f10e01e2b98e9a8852d80f334b5
nsc-g-inagaki <63277586+nsc-g-inagaki@users.noreply.github.com>
2020-04-14 17:50:29 +0900

    レビュー

--- js/even-odd.js
+++ js/even-odd.js
@@ -17,3 +17,15 @@ checkNumber(5);
 
 // コンソールに「偶数です」と表示される
 checkNumber(2);
+
+//if..elseを使う場合は、ifの条件が満たされない場合elseの方が実行されるので
+//Line10のelseの( (num % 2) != 0) が必要ないです。
+/*
+  こんな感じに書くといいですよ
+  if((num % 2) == 0) {
+    console.log('偶数です');
+  } else {
+    console.log('奇数');
+  }
+
+*/
