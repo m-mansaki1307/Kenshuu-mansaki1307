47803542c2f9c8da18d198817bd5009388992164
m-mansaki1307 <63280703+m-mansaki1307@users.noreply.github.com>
2020-04-21 11:20:26 +0900

    Initial commit

--- 自習　課題/jishuu.js
+++ 自習　課題/jishuu.js
@@ -2,44 +2,57 @@
 
 // 指定する数字 
 
-function fibonatchi(n,count){
+// function fibonatchi(n,count){
  
-  for (let i=0; i<count; i++) {
-    result =(n-1)+(n-2); //フィボナッチがよくわからない
-    console.log(result);
-  } 
-}
-fibonatchi(1,10);
+//   for (let i=0; i<count; i++) {
+//     result =(n-1)+(n-2); //フィボナッチがよくわからない
+//     console.log(result);
+//   } 
+// }
+// fibonatchi(1,10);
 
 // 入力した数字が素数かどうかチェックする。
 
-// function sosuu(num) {
-//   if(num == 2) {
-//     console.log('素数です');
-//     return true; 
-    
-// } else {
-//   for(i=2; i<num; i++){
-//     if(num % 1 == 0) {
-//       console.log('素数ではない');
-//       return false;
+function sosuu(num) {
+  if(num == 2) {
+    console.log('素数です');
 
-//     } else if(i + 1 == num) {
-//       console.log('素数です');
-//       return true;
-//     }
-//   }
-//   console.log(num);
-//  }
-// }
-// sosuu(12)
+  } else if(num == 1) {
+    console.log('素数ではない');
+   
+  } else {
+      for(i=2; i<num; i++){
+       if(num % i == 0) {
+          console.log('素数ではない');
+          break;
+    
+       } else if(i + 1 == num) {
+          console.log('素数です');
+          break;
+       }
+    }
+  }
+}
+sosuu(1)
 
 // 配列の中の値を昇順にする
-// var array = [19,36,7];
+var array = [19,36,7];
 
-// arr.sort(
-//   (a,b) => {
-//     return(a < b ? -1 : 1);
-//   }
-// );
-// console.log(arr);
+
+array.sort(
+  function(a,b)  {
+    return(a > b ? -1 : 1);
+  }
+);
+console.log(array);
+
+// 配列の中の値を降順にする
+var array = [100,24,321];
+
+array.sort(
+  function(a,b)  {
+    return(a < b ? -1 : 1);
+  }
+);
+console.log(array);
+
